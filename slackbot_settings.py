cb161528ca0d7b4dbd03b9963c894ad0037c47b4
nagai shinya <s.nagai@datumstudio.jp>
2018-02-04 12:20:21 +0900

    ファイルからAPI tokenを読み込む

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,6 +1,19 @@
-import getpass
+import os
 
-API_TOKEN = getpass.getpass('slackbot aip-token: ')
+API_TOKEN_FILE = './api_token.txt'
+
+
+def read_api_token():
+    assert os.path.exists(API_TOKEN_FILE), \
+        'カレントディレクトリにapi_tokenを書いたテキストファイル(api_token.txt)を置いてください'
+
+    with open(API_TOKEN_FILE, 'r') as f:
+        api_token = f.read()
+
+    return api_token.replace('\n', '')
+
+
+API_TOKEN = read_api_token()
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "?"
